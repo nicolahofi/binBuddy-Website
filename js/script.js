467e0b465a61d24385e3fa78f4eb94c934e6d504
nicolahofi <nicola.hofmaenner@gmail.com>
2023-12-14 15:46:41 +0100

    added map

--- js/script.js
+++ js/script.js
@@ -1,28 +1,89 @@
 function initMap(){
 
+  // Map option
+
   var options = {
-    center: { lat: 46.851949140622025, lng: 9.53223487821476 },
-    zoom: 14
-   }
+      center: { lat: 46.851949140622025, lng: 9.53223487821476 },
+      zoom: 14,
+      mapId: "801170992c8617e7"
+  }
 
-   map =new google.maps.Map(document.getElementById('map'), options);
+  //New Map
+  map = new google.maps.Map(document.getElementById("map"),options)
 
-//Marker
+  //listen for click on map location
 
-const marker = new google.maps.Marker({
-  position: { lat: 46.85719987945518, lng: 9.518000876366889 },
-  map: map,
-  icon: "img/glas_icon.png" 
-});
-
-//Info-Fenster
-
-  const detailWindow = new google.maps.InfoWindow({
-    content: '<h2>Recyclinghof</h2>'
-  });
-
-  marker.addListener('click', () => {
-    detailWindow.open(map, marker);
+  google.maps.event.addListener(map, "click", (event) => {
+      //add Marker
+      addMarker({location:event.latLng});
   })
 
+  //Add Markers to Array
+
+  let MarkerArray = [ 
+    
+    {
+      location:{lat: 46.85719987945518, lng: 9.518000876366889}, 
+      imageIcon: "img/karton_icon.png",
+      content: `<h3>Multisammelstelle Industriestrasse</h3>`
+    },
+
+    {
+      location:{lat: 46.85101329014681, lng: 9.537040027852614},
+      imageIcon: "img/glas_icon.png", 
+      content: `<h3>Multisammelstelle Reichsgasse</h3>`
+    },
+
+    {
+      location:{lat: 38.5411, lng: -0.1225},content: `<h2>Benidorm City</h2>`
+    }
+
+
+
+  ]
+
+  // loop through marker
+  for (let i = 0; i < MarkerArray.length; i++){
+      addMarker(MarkerArray[i]);
+
   }
+
+  // Add Marker
+
+  function addMarker(property){
+
+      const marker = new google.maps.Marker({
+          position:property.location,
+          map:map,
+          //icon: property.imageIcon
+          });
+
+          // Check for custom Icon
+
+          if(property.imageIcon){
+              // set image icon
+              marker.setIcon(property.imageIcon)
+          }
+
+          if(property.content){
+
+          const detailWindow = new google.maps.InfoWindow({
+          content: property.content
+  });
+  
+  marker.addListener("mouseover", () =>{
+      detailWindow.open(map, marker);
+  })
+}
+
+       
+
+
+
+  }
+
+  
+
+
+
+}
